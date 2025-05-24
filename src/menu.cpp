e66b66196fc02ee50fe760acbbca5e872cbc2aab
WilliamLebris <williamtissi2006@gmail.com>
2025-05-23 23:05:10 -0700

    nothing has changed, still using enums, i just change the function to call since i created some new file and im using classed now

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,29 +1,14 @@
-//
-// Created by William Tissi on 5/1/25.
-//
-
-// since we need to propose the menu at least once before the user say he doesn't want to see it anymore, I thought that using do while loop is the best fit because it does an action at least once before checkinng any conditon. And we also need to propose this menu to our user constanly to propose to the user our available feature
-
-// I also created a feedback feature using ternary operator to be creative.
-//goal 1: stand out of the from the crowd to be special
-
-
-// since our menu function use others function, it is important to have their library (import).
-//
-// Created by William Tissi on 5/1/25.
-//
-
 #include "../include/menu.h"
-#include "../include/addOrder.h"
-#include "../include/displayOrders.h"
-#include "../include/searchOrder.h"
-#include "../include/deleteOrder.h"
-#include "../include/getValidInt.h"
+#include "../include/order_system.h"
+#include "../include/file_manager.h"
 #include "../include/UserDatabase.h"
-#include "../include/printStoreOrders.h"
+#include "../include/getValidInt.h"
+#include "../include/addOrder.h"  // Include addOrder functionality
 #include <iostream>
 #include <thread>
-// here i am basically just using enums instead of switch
+#include <memory>
+
+
 using namespace std;
 
 enum MenuOption {
@@ -38,48 +23,79 @@ enum MenuOption {
 };
 
 void menuDeclaration::menu() {
+    auto fileManager = make_unique<CSVFileManager>();
     int choice;
     int feedbackResponse;
+
     do {
         cout << "\n===== MENU =====\n";
         cout << "1. Add Order\n";
         cout << "2. Display Orders\n";
         cout << "3. Search Order\n";
         cout << "4. Delete Order\n";
-        cout << "5. Print all the orders since the store opened\n";
+        cout << "5. Print all store orders\n";
         cout << "6. Add a new user\n";
-        cout << "7. Log in as another user\n";
+        cout << "7. Log in\n";
         cout << "8. Exit\n";
         cout << "====================\n";
         cout << "Enter your choice: ";
         choice = intValidation::getValidInt(1, 8);
 
         switch (choice) {
-            case ADD_ORDER: addOrderDeclaration::addOrder(); break;
-            case DISPLAY_ORDERS: displayOrderDeclaration::displayOrders(); break;
-            case SEARCH_ORDER: searchOrderDeclaration::searchOrder(); break;
-            case DELETE_ORDER: deleteOrderDeclaration::deleteOrder(); break;
-            case PRINT_STORE_ORDERS: readAllOrdersFromBinary(); break;
-            case ADD_USER: addNewUser(); break;
-            case LOGIN_USER: login(); break;
+            case ADD_ORDER:
+                addOrderDeclaration::addOrder();
+                break;
+
+            case DISPLAY_ORDERS:
+                fileManager->displayOrders();
+                break;
+
+            case SEARCH_ORDER:
+                fileManager->searchOrder();
+                break;
+
+            case DELETE_ORDER: {
+                string name;
+                cout << "Enter customer name to delete: ";
+                getline(cin, name);
+                if (fileManager->deleteOrder(name)) {
+                    cout << "Order deleted successfully.\n";
+                } else {
+                    cout << "Order not found.\n";
+                }
+                break;
+            }
+
+            case PRINT_STORE_ORDERS:
+                fileManager->printAllOrders();
+                break;
+
+            case ADD_USER:
+                UserDatabase::addNewUser();
+                break;
+
+            case LOGIN_USER:
+                UserDatabase::login();
+                break;
+
             case EXIT_PROGRAM:
-                cout << "Exiting program...\n" << endl;
+                cout << "Exiting program...\n";
                 cout << "Your experience is valuable\n";
-                cout << "Do you mind giving us some feedback about your experience with this program?\n";
-                cout << "Enter 1 for yes or 2 for no\n";
+                cout << "Do you mind giving feedback? (1=yes, 2=no): ";
                 feedbackResponse = intValidation::getValidInt(1, 2);
 
                 if (feedbackResponse == 1) {
-                    std::thread feedbackThread([]() {
-                        cout << "How would you rate the program on a scale of 0 to 10\n";
-                        int rateExperience = intValidation::getValidInt(0, 10);
-                        cout << "***    Thank you !!!    ***\n" << endl;
+                    thread feedbackThread([]() {
+                        cout << "Rate the program (0-10): ";
+                        int rating = intValidation::getValidInt(0, 10);
+                        cout << "*** Thank you! ***\n";
                     });
                     feedbackThread.join();
-                } else {
-                    cout << "Okay !!\n";
                 }
                 break;
+
+            default:
+                cout << "Invalid choice. Please try again.\n";
         }
     } while (choice != EXIT_PROGRAM);
 }
