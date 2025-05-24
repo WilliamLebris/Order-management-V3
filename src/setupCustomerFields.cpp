554208903151fffb28da6c976c27d03cdac3810f
WilliamLebris <williamtissi2006@gmail.com>
2025-05-23 22:51:30 -0700

    it works the same way like in project 2, i just that im saving the customer field now into the class orderSystem

--- src/setupCustomerFields.cpp
+++ src/setupCustomerFields.cpp
@@ -2,31 +2,34 @@
 // Created by William Tissi on 5/1/25.
 //
 #include "../include/setupCustomerFields.h"
-#include "../include/globalVariable.h"
+#include "../include/order_system.h"
 #include "../include/getValidInt.h"
 #include <iostream>
 using namespace std;
 
 //Setup Customer Fields (e.g., Name, Phone). I use 5 max the max for this program but we can always increase it
+// setupCustomerFields.cpp
 void setCustomerDeclaration::setupCustomerFields() {
-    cout << "How many details should customers provide? (e.g., Name, Phone) Max 5: ";
-    numFields = intValidation::getValidInt(1, 5);  // make sure it is between 1 and 5
+    cout << "How many details should customers provide? (e.g., Name, Phone) Max "
+         << OrderSystem::MAX_CUSTOMER_FIELDS << ": ";
+    int numFields = intValidation::getValidInt(1, OrderSystem::MAX_CUSTOMER_FIELDS);
 
-
-    // we can't use for ranged based loop here because we need to stop after the loop depending on the user answer
+    vector<string> fields;
     for (int i = 0; i < numFields; i++) {
         while (true) {
+            string fieldName;
             cout << "Enter field #" << i + 1 << " (e.g., Name, Phone, ID #): ";
-            getline(cin, customerFields[i]);
-            //the order details has to be either a string or and int, like for instance, name, or ID, we can have something like nd34f, it doesn't means anything
+            getline(cin, fieldName);
 
-            int result = intValidation::validateLettersOrDigits(customerFields[i]);
+            int result = intValidation::validateLettersOrDigits(fieldName);
 
             if (result == 1) {
                 cout << "Accepted: All letters (e.g., Name).\n";
+                fields.push_back(fieldName);
                 break;
             } else if (result == 2) {
                 cout << "Accepted: All digits (e.g., ID/Phone).\n";
+                fields.push_back(fieldName);
                 break;
             } else {
                 cout << "Invalid input. Please enter only letters or only digits.\n";
@@ -34,4 +37,5 @@ void setCustomerDeclaration::setupCustomerFields() {
         }
     }
 
+    OrderSystem::setCustomerFields(fields);
 }
